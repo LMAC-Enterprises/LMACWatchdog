4402698d578d30864e0f056be287c361e9f9acd9
quantumg <coding4food@mail.de>
2022-01-27 14:16:36 +0100

    Added iMac typo detection.

--- monitoringSystem/agents/LMACBeneficiaryAgent.py
+++ monitoringSystem/agents/LMACBeneficiaryAgent.py
@@ -1,6 +1,8 @@
 from abc import ABC
+from typing import Tuple
 
-from inputOutput.HiveNetwork import HiveComment
+from actionSystem.ActionHandling import PolicyAction
+from services.HiveNetwork import HiveComment
 from monitoringSystem.MonitoringAgency import Agent
 from reportingSystem.Reporting import SuspiciousActivityReport, SuspiciousActivityLevel
 
@@ -18,31 +20,37 @@ class LMACBeneficiaryAgent(Agent, ABC):
         self._minimumBenefication = rules['minimumBenefication']
         self._requiredBeneficiary = rules['requiredBeneficiary']
 
-    def onSuspicionQuery(self, post: HiveComment) -> SuspiciousActivityReport:
+    def onSuspicionQuery(self, post: HiveComment) -> Tuple[SuspiciousActivityReport, PolicyAction]:
 
-        if post.author != 'shaka':
+        if post.author == 'shaka':
+            return None, None
+
+        if 'imac' in post.cachedBeneficiaries:
             return SuspiciousActivityReport(
                 post.author,
                 post.permlink,
                 self._agentId,
-                SuspiciousActivityLevel.UNSUSPICIOUS,
-                'Unknown type of post.'
-            )
+                SuspiciousActivityLevel.WARNING,
+                'iMac typo in "@{requiredBeneficiary}" beneficiary.'.format(
+                    requiredBeneficiary=self._requiredBeneficiary)
+            ), None
 
-        if self._requiredBeneficiary in post.beneficiaries.keys():
-            if post.beneficiaries[self._requiredBeneficiary] < self._minimumBenefication:
+        if self._requiredBeneficiary in post.cachedBeneficiaries.keys():
+            if post.cachedBeneficiaries[self._requiredBeneficiary] < self._minimumBenefication:
                 return SuspiciousActivityReport(
                     post.author,
                     post.permlink,
                     self._agentId,
                     SuspiciousActivityLevel.WARNING,
-                    'Insufficient beneficiary weight set for @' + self._requiredBeneficiary
-                )
+                    'Insufficient beneficiary weight set for @{requiredBeneficiary}.'.format(requiredBeneficiary=self._requiredBeneficiary)
+                ), None
+        else:
+            return SuspiciousActivityReport(
+                post.author,
+                post.permlink,
+                self._agentId,
+                SuspiciousActivityLevel.WARNING,
+                'Beneficiary not set for @{requiredBeneficiary}.'.format(requiredBeneficiary=self._requiredBeneficiary)
+            ), None
 
-        return SuspiciousActivityReport(
-            post.author,
-            post.permlink,
-            self._agentId,
-            SuspiciousActivityLevel.UNSUSPICIOUS,
-            ''
-        )
+        return None, None
