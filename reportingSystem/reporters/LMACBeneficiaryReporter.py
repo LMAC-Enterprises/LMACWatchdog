60a31349f0f8e4a80e5203e8d955a5dcab997064
quantumg <coding4food@mail.de>
2022-01-27 14:17:25 +0100

    Removed LMACBeneficiaryReporter.py

--- reportingSystem/reporters/LMACBeneficiaryReporter.py
+++
@@ -1,9 +0,0 @@
-from reportingSystem.Reporting import Reporter, SuspiciousActivityReport
-
-
-class LMACBeneficiaryReporter(Reporter):
-    def onNewReportAvailable(self, report: SuspiciousActivityReport):
-        pass
-
-    def onStart(self, arguments: dict):
-        pass
