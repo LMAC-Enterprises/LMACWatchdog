5a9f3d7dae31c409456b8e2893a8746a1dd95f01
quantumg <coding4food@mail.de>
2022-01-27 14:19:47 +0100

    Integrated Discord reporting.

--- main.py
+++ main.py
@@ -1,41 +1,76 @@
 import argparse
 
 from Configuration import Configuration
+from actionSystem.ActionHandling import PolicyActionSupervisor
 from monitoringSystem.agents import LMACBeneficiaryAgent, SourceBlacklistAgent, SuspectHunterAgent
 from monitoringSystem.MonitoringAgency import AgentSupervisor
 from reportingSystem.Reporting import ReportDispatcher
-from reportingSystem.reporters import LogReporter
+from reportingSystem.reporters import LogReporter, DiscordReporters
+from services.Discord import DiscordDispatcher
+from services.HiveNetwork import HiveWallet
+from services.Registry import RegistryHandler
 
 EXITCODE_OK: int = 0
 EXITCODE_ERROR: int = 1
 
 
+def _onAgentSupervisorProgress(reachedTask: str):
+    print(reachedTask)
+
+
+def _initialize() -> int:
+    print('Create a new local wallet first.')
+    hiveWalletPassword = input('Password:')
+    hivePostingKey = input('Posting key:')
+    print('Initialisation finished!')
+
+
 def main(arguments: dict) -> int:
+    if not arguments['initialize'] and not (
+            'discordToken' in arguments.keys() and 'walletPassword' in arguments.keys()):
+        return EXITCODE_ERROR
+
+    if arguments['initialize']:
+        return _initialize()
+
+    if not HiveWallet.unlock(arguments['walletPassword']):
+        print('Error. Wrong wallet password.')
+        return EXITCODE_ERROR
+
+    registryHandler = RegistryHandler()
+
+    reportDispatcher = ReportDispatcher({
+            DiscordReporters.ViolationReporter: Configuration.violationReporterSettings,
+            LogReporter.LogReporter: {}
+        }
+    )
+
+    policyActionSupervisor = PolicyActionSupervisor({})
+
     agentSupervisor = AgentSupervisor(
-        Configuration.agentSupervisor['hiveCommunityId'],
-        Configuration.agentSupervisor['hiveCommunityTag'], {
-            LMACBeneficiaryAgent.LMACBeneficiaryAgent: Configuration.lmacBeneficiaryAgent,
+        Configuration.agentSupervisorSettings['hiveCommunityId'],
+        Configuration.agentSupervisorSettings['hiveCommunityTag'], {
+            LMACBeneficiaryAgent.LMACBeneficiaryAgent: Configuration.lmacBeneficiaryAgentRules,
             SourceBlacklistAgent.SourceBlacklistAgent: Configuration.sourceBlacklistAgentRules,
-            SuspectHunterAgent.SuspectHunterAgent: Configuration.suspectHunterAgent
-        })
+            SuspectHunterAgent.SuspectHunterAgent: Configuration.suspectHunterAgentRules,
+        },
+        policyActionSupervisor,
+        reportDispatcher,
+        _onAgentSupervisorProgress
+    )
 
     try:
         agentSupervisor.startSearching()
     except IOError:
+        print('Could not load Hive posts.')
         return EXITCODE_ERROR
 
-    reportDispatcher = ReportDispatcher({
-        # PostCommentReporter.PostCommentReporter: {'hiveWalletPassword': 'xyz'},
-        # DiscordReporter.DiscordReporter: {'discordToken': ''},
-        LogReporter.LogReporter: {}
-    }
-    )
+    agentSupervisor.finishMonitoringCycle()
 
-    reports = agentSupervisor.getReports()
-    if len(reports) == 0:
-        return EXITCODE_OK
+    registryHandler.saveAll()
 
-    reportDispatcher.sendReports(reports)
+    discordDispatcher = DiscordDispatcher()
+    discordDispatcher.processMessages(arguments['discordToken'])
 
     return EXITCODE_OK
 
@@ -43,12 +78,23 @@ def main(arguments: dict) -> int:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.description = 'Monitors a community for contributions that do not comply with the rules.'
-    """parser.add_argument(
-        '-a',
+    parser.add_argument(
+        '-discordToken',
         type=str,
-        help='Hive url of a post. Syntax: \'contestthumbnailer.py -a "@user/permlink"\'.',
-        required=True
-    )"""
+        help='Discord token. -discordToken [TOKEN]',
+        required=False
+    )
+    parser.add_argument(
+        '-walletPassword',
+        type=str,
+        help='Local wallet password. -discordToken [TOKEN]',
+        required=False
+    )
+    parser.add_argument(
+        '-initialize',
+        help='Local wallet password. -discordToken [TOKEN]',
+        required=False
+    )
 
     exit(
         main(
