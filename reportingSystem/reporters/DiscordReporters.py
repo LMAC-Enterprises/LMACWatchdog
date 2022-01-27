6dc829ac8cf62c191e8970e4f11f6e5d7d916585
quantumg <coding4food@mail.de>
2022-01-27 14:13:19 +0100

    Added Discord reporter ViolationReporter.

---
+++ reportingSystem/reporters/DiscordReporters.py
@@ -0,0 +1,28 @@
+from reportingSystem.Reporting import Reporter, SuspiciousActivityReport, SuspiciousActivityLevel
+from services.Discord import DiscordDispatcher
+
+
+class ViolationReporter(Reporter):
+    _discordDispatcher: DiscordDispatcher
+    _settingsByLevel: dict
+
+    def __init__(self):
+        self._discordDispatcher = DiscordDispatcher()
+        self._settingsByLevel = {}
+
+    def onNewReportAvailable(self, report: SuspiciousActivityReport):
+        if report.activityLevel not in self._settingsByLevel:
+            return
+
+        self._discordDispatcher.enterChatroom(self._settingsByLevel[report.activityLevel]['discordTargetChatroom'])
+        self._discordDispatcher.enqueueMessage(
+            '{report}\nhttps://peakd.com/@{author}/{permlink}'.format(
+                report=report.description,
+                permlink=report.permlink,
+                author=report.author
+            )
+        )
+
+    def onStart(self, arguments: dict):
+        self._settingsByLevel = arguments['settingsByLevel']
+
