913003bdcf69957ebada4dee1cbecfbf3bcec21e
quantumg <coding4food@mail.de>
2022-08-27 23:41:07 +0200

    Added BlacklisedUserReporter.

--- reportingSystem/reporters/DiscordReporters.py
+++ reportingSystem/reporters/DiscordReporters.py
@@ -1,4 +1,4 @@
-from reportingSystem.Reporting import Reporter, SuspiciousActivityReport
+from reportingSystem.Reporting import Reporter, SuspiciousActivityReport, SuspiciousActivityLevel
 from services.Discord import DiscordDispatcher
 
 
@@ -25,3 +25,27 @@ class ViolationReporter(Reporter):
 
     def onStart(self, arguments: dict):
         self._settingsByLevel = arguments['settingsByLevel']
+
+
+class BlacklistedUserReporter(Reporter):
+    _discordDispatcher: DiscordDispatcher
+
+    def __init__(self):
+        self._discordDispatcher = DiscordDispatcher()
+        self._reportInDiscordChannelId = 0
+
+    def onNewReportAvailable(self, report: SuspiciousActivityReport):
+        if report.activityLevel != SuspiciousActivityLevel.BLACKLISTED_USER:
+            return
+
+        self._discordDispatcher.enterChatroom(self._reportInDiscordChannelId)
+        self._discordDispatcher.enqueueMessage(
+            '{report}\nhttps://peakd.com/@{author}/{permlink}'.format(
+                report=report.description,
+                permlink=report.permlink,
+                author=report.author
+            )
+        )
+
+    def onStart(self, arguments: dict):
+        self._reportInDiscordChannelId = arguments['reportInDiscordChannelId']
